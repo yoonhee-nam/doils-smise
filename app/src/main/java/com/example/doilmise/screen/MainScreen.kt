ef29cdc0e8e44dab9b7539e7af868c24746b9e81
yoon <flvps123@gmail.com>
2024-11-09 13:32:42 +0900

    local commit

--- app/src/main/java/com/example/doilmise/screen/MainScreen.kt
+++ app/src/main/java/com/example/doilmise/screen/MainScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
